56739d023a5f95b9f38ab7dbd859a09795a3792d
ilya_belyshev <ikoldik@gmail.com>
2023-05-18 00:11:00 +0300

    deploy

--- database.sql
+++ database.sql
@@ -1,6 +1,15 @@
-CREATE DATABASE perntodo;
+CREATE DATABASE blog;
 
-CREATE TABLE todo(
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    text VARCHAR(255)
-)
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL -- храните хеш пароля, а не сам пароль
+);
+
+CREATE TABLE articles (
+    id SERIAL PRIMARY KEY,
+    publication_date TIMESTAMP NOT NULL,
+    text TEXT NOT NULL,
+    user_id INTEGER REFERENCES users(id)
+);
--- index.js
+++ index.js
@@ -226,6 +226,7 @@ app.use(express.static(path.join(__dirname, "build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
+const port = 5555;
 app.listen(port, () => {
-  console.log(`server start, port ${port}`)
+  console.log(`Server is running on port ${port}`)
 })
